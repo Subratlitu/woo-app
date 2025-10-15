8a328c3c8458bc2f9e24ad11ee4f6ea3e443755c
subrat <subratnayak940@gmail.com>
2025-10-15 15:40:35 +0530

    feat:env-added-for-fe

--- frontend/src/api/productAPI.js
+++ frontend/src/api/productAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const PRODUCT_SERVICE_URL = "http://localhost:4000/api/products";
+const PRODUCT_SERVICE_URL = import.meta.env.VITE_PRODUCT_SERVICE_URL;
 
 export const fetchProducts = async (page = 1, limit = 10) => {
   const res = await axios.get(PRODUCT_SERVICE_URL, { params: { page, limit } });
